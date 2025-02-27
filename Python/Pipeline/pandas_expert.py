257126a1e822bebcd628c22c16faa5c59cfa9caa
Mateus de Assis <85937501+massis93@users.noreply.github.com>
2025-02-27 16:43:28 -0300

    Update pandas_expert.py

--- Python/Pipeline/pandas_expert.py
+++ Python/Pipeline/pandas_expert.py
@@ -1,24 +1,33 @@
 # %%
+#importando bibliotecas
 import pandas as pd
 import os
 
 # %%
+#definindo a raiz do path
 path = '../../Desbravando_Pandas/desbravando-pandas-main/data/ipea/'
 
 # %%
+#criando função etl
 def etl(name):
     return((pd.read_csv(path+name, sep = ';')
         .set_index(['cod', 'nome', 'período'])
         .rename(columns = {'valor': name})))
 
 # %%
+#listando os diretorios e salvando em arquivos
 arquivos = os.listdir(path)
 arquivos
 # %%
+#salvando dataframe em lista
 lista = []
 for arquivo in arquivos:
     lista.append(etl(arquivo))
 
 # %%
-pd.concat(lista, axis=1).reset_index()
+#concatenando todos os dataframes
+df = pd.concat(lista, axis=1).reset_index()
 # %%
+#exportando para csv
+df.to_csv('concatenado.csv', sep=',')
+
